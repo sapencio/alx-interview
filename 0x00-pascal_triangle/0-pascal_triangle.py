d28278f28a18227b35c3be23f445d1b86e9a9051
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2022-12-20 16:35:01 +0300

    Pascal Triangle

--- 0x00-pascal_triangle/0-pascal_triangle.py
+++ 0x00-pascal_triangle/0-pascal_triangle.py
@@ -2,20 +2,19 @@
 '''A module for working with Pascal's triangle.
 '''
 
+
 def pascal_triangle(n):
-	'''Creates a list of lists of integers representing the Pascal's triangle of a given integer.
-  '''
-	a = [[] for i in range(n)]
-	if type(n) is not int or n <= 0:
-		return a
-	for i in range(n):
-		for j in range(i + 1):
-			if (j < i):
-				if (j == 0):
-					a[i].append(1)
-				else:
-					a[i].append(a[i - 1][j] + a[i - 1][j - 1])
-			elif (j == i):
-				a[i].append(1)
-	
-	return a
+    '''Creates a list of lists of integers representing the Pascal's triangle of a given integer.
+    '''
+    triangle = []
+    if type(n) is not int or n <= 0:
+        return triangle
+    for i in range(n):
+        line = []
+        for j in range(i + 1):
+            if j == 0 or j == i:
+                line.append(1)
+            elif i > 0 and j > 0:
+                line.append(triangle[i - 1][j - 1] + triangle[i - 1][j])
+        triangle.append(line)
+    return triangle
