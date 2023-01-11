3055e51c0a8591e4f51cd67c27eabfa43875566b
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-01-11 15:43:18 +0300

    Minimum Operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -7,12 +7,29 @@ def minOperations(n):
     '''Computes the fewest number of operations needed to result
     in exactly n H characters.
     '''
-    numOp = 0
-    while n != 0: 
-        if n % 2 == 0: 
-            numOp += 1 
-            n /= 2 
-        else: 
-            numOp += 1 
-        n -= 1 
-    return numOp
+    if not isinstance(n, int):
+        return 0
+    ops_count = 0
+    clipboard = 0
+    done = 1
+    # print('H', end='')
+    while done < n:
+        if clipboard == 0:
+            # init (the first copy all and paste)
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif n - done > 0 and (n - done) % done == 0:
+            # copy all and paste
+            clipboard = done
+            done += clipboard
+            ops_count += 2
+            # print('-(11)->{}'.format('H' * done), end='')
+        elif clipboard > 0:
+            # paste
+            done += clipboard
+            ops_count += 1
+            # print('-(01)->{}'.format('H' * done), end='')
+    # print('')
+    return ops_count
