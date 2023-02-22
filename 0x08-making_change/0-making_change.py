2c3473c8712c7759bbc76081f534b463b108433d
Tibebe Solomon <62059876+sapencio@users.noreply.github.com>
2023-02-22 15:16:50 +0300

    Making changes

--- 0x08-making_change/0-making_change.py
+++ 0x08-making_change/0-making_change.py
@@ -2,26 +2,27 @@
 """ Change making module.
 """
 
+
 def makeChange(coins, total):
-	""" Determines the fewest number of coins needed to meet a given 
-	amount total when given a pile of coins of different values.
-	"""
-	if total <= 0:
-		return 0
+		""" Determines the fewest number of coins needed to meet a given
+		amount total when given a pile of coins of different values.
+		"""
+		if total <= 0:
+				return 0
+		
+		rem = total
+		coins_count = 0
+		coin_idx = 0
+		sorted_coins = sorted(coins, reverse=True)
+		n = len(coins)
+		while rem > 0:
+				if coin_idx >= n:
+						return -1
 
-	# Initialize an array to store the fewes number of coins needed
-	# for each amount from 0 to total
-	dp = [float('inf') * (total + 1)]
-
-	# Set the fewest number of coins needed for 0 as 0
-	dp[0] = 0
-
-	# Iterate through each coin value for coin in coins
-	# For each coin value, update the fewest number of coins needed for
-	# each amount from the coin value to total
-	for i in range(coins, total + 1):
-		dp[i] = min(dp[i], dp[i - coins] + 1)
-
-	# If thefewest number of coins nedded for the total is still infinity,
-	# it means it cannot be met by any number of coins in the list
-	return dp[total] if dp[total] != float('inf') else -1
+				if rem - sorted_coins[coin_idx] >= 0:
+						rem -= sorted_coins[coin_idx]
+						coins_count += 1
+				else:
+						coin_idx += 1
+		
+		return coins_count
